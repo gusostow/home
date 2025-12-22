685d2ea39e4dca7b5b1dde4f8743d3295215b466
Augustus Ostow <ostowster@gmail.com>
2025-12-22 12:02:33 -0500

    Remove pulseaudio

--- hosts/server-system.nix
+++ hosts/server-system.nix
@@ -57,7 +57,6 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
