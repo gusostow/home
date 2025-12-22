9f3a72c79ac40684c097b110b16f2bf11b8b8640
Augustus Ostow <ostowster@gmail.com>
2025-12-22 14:30:12 -0500

    Downloads fixes

--- modules/plex/downloads.nix
+++ modules/plex/downloads.nix
@@ -5,24 +5,21 @@
     # qBittorrent with web UI
     services.qbittorrent = {
       enable = true;
-      port = 8080;
+      webuiPort = 8080;
       openFirewall = true;
+      user = "qbittorrent";
+      group = "media";
+      profileDir = "/space/config/qbittorrent";
+    };
+
+    # Create qbittorrent user
+    users.users.qbittorrent = {
+      isSystemUser = true;
       group = "media";
     };
 
     # Ensure qBittorrent starts after /space is mounted
     systemd.services.qbittorrent.after = [ "space.mount" ];
     systemd.services.qbittorrent.requires = [ "space.mount" ];
-
-    # Configure qBittorrent state directory to be on /space
-    systemd.services.qbittorrent.serviceConfig = {
-      StateDirectory = lib.mkForce "qbittorrent";
-      StateDirectoryMode = "0750";
-    };
-
-    # Create symlink from default location to /space
-    systemd.tmpfiles.rules = [
-      "L+ /var/lib/qbittorrent - - - - /space/config/qbittorrent"
-    ];
   };
 }
