6f134998a3ba471db142457048fe4662eab6d8b8
Augustus Ostow <ostowster@gmail.com>
2026-01-04 17:56:19 -0500

    Change hostname to ultan.home

--- hosts/ultan-system.nix
+++ hosts/ultan-system.nix
@@ -12,7 +12,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "ultan"; # Define your hostname.
+  networking.hostName = "ultan.home"; # Define your hostname.
 
   # Static IP configuration
   networking = {
