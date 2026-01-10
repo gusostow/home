a54c0aac5357647097e0b5994e9ca18902e0535c
Augustus Ostow <ostowster@gmail.com>
2026-01-10 17:37:44 -0500

    Add netstat to ultan

--- hosts/ultan.nix
+++ hosts/ultan.nix
@@ -7,10 +7,10 @@
 
   programs.home-manager.enable = true;
 
-  # Server-specific packages (if any)
   home.packages = with pkgs; [
     dig
     ethtool
     unixtools.ifconfig
+    unixtools.netstat
   ];
 }
