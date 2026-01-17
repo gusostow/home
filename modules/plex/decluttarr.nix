40874d8b25a6d6cd913399123cb1d1627486ca52
Augustus Ostow <ostowster@gmail.com>
2026-01-17 18:08:18 -0500

    Use install from coreutils

--- modules/plex/decluttarr.nix
+++ modules/plex/decluttarr.nix
@@ -39,7 +39,7 @@ in
         Group = "media";
         WorkingDirectory = "/var/lib/decluttarr";
         ExecStartPre = ''
-          install -m 0640 -o decluttarr -g media ${configPath} /var/lib/decluttarr/config.yaml
+          ${pkgs.coreutils}/bin/install -m 0640 -o decluttarr -g media ${configPath} /var/lib/decluttarr/config.yaml
         '';
         # use flake output from this repo
         ExecStart = ''
