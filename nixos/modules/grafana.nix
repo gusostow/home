534b46a8bf4cde23f0954220a4c6991941346a68
Augustus Ostow <ostowster@gmail.com>
2026-02-01 16:42:22 -0500

    Set up node exporter dashboard

--- nixos/modules/grafana.nix
+++ nixos/modules/grafana.nix
@@ -5,6 +5,18 @@
   ...
 }:
 
+let
+  # Node Exporter Full dashboard (ID: 1860)
+  nodeExporterDashboard = pkgs.fetchurl {
+    url = "https://grafana.com/api/dashboards/1860/revisions/37/download";
+    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+  };
+
+  dashboardsDir = pkgs.runCommand "grafana-dashboards" { } ''
+    mkdir -p $out
+    cp ${nodeExporterDashboard} $out/node-exporter-full.json
+  '';
+in
 {
   services.grafana = {
     enable = true;
@@ -22,6 +34,12 @@
           type = "prometheus";
           url = "http://localhost:9090";
           isDefault = true;
+        }
+      ];
+      dashboards.settings.providers = [
+        {
+          name = "default";
+          options.path = dashboardsDir;
         }
       ];
     };
