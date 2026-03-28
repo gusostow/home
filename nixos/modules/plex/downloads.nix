23f1592dbc8ae90193dd09713f65eacc8f07695a
Augustus Ostow <ostowster@gmail.com>
2026-03-28 17:14:56 -0400

    Commit manual configuration options so they persist

--- nixos/modules/plex/downloads.nix
+++ nixos/modules/plex/downloads.nix
@@ -16,13 +16,29 @@
       group = "media";
       profileDir = "/space/config/qbittorrent";
 
-      # Configure download paths
+      # configure download paths and settings
       serverConfig = {
+        LegalNotice.Accepted = true;
+
         Preferences = {
           Downloads = {
             SavePath = "/space/downloads/complete";
             TempPath = "/space/downloads/incomplete";
             TempPathEnabled = true;
+          };
+
+          # webui authentication
+          WebUI = {
+            Username = "admin";
+            # generate with: python3 -c "import hashlib, os, base64; password = input('Password: '); salt = os.urandom(16); dk = hashlib.pbkdf2_hmac('sha512', password.encode(), salt, 100000); print(f'@ByteArray({base64.b64encode(salt).decode()}:{base64.b64encode(dk).decode()})')"
+            Password_PBKDF2 = "@ByteArray(NvQWm8rxk05OjW8Sa3isqg==:qAcFy2jgRyOnZjPMUigESXs7CkU9aK341nRC00SOPIb7vRT+Gjkt8NBiWsYKQygVEEfnPN2DSQtNAy4BuaBtIg==)";
+          };
+
+          # unlimited active downloads/uploads
+          Queueing = {
+            MaxActiveDownloads = -1; # -1 = unlimited
+            MaxActiveUploads = -1; # -1 = unlimited
+            MaxActiveTorrents = -1; # -1 = unlimited
           };
         };
       };
