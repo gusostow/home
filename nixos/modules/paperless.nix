0c23b72564741758e8a026dda4e6bb08b60ef522
Augustus Ostow <ostowster@gmail.com>
2026-07-05 15:48:45 -0400

    INcrease log level

--- nixos/modules/paperless.nix
+++ nixos/modules/paperless.nix
@@ -73,14 +73,8 @@
         # iOS compatibility
         "server min protocol" = "SMB2";
         "server max protocol" = "SMB3";
-        "ea support" = "yes";
-        "vfs objects" = "fruit streams_xattr";
-        "fruit:metadata" = "stream";
-        "fruit:model" = "MacSamba";
-        "fruit:posix_rename" = "yes";
-        "fruit:veto_appledouble" = "no";
-        "fruit:wipe_intentionally_left_blank_rfork" = "yes";
-        "fruit:delete_empty_adfiles" = "yes";
+        # logging
+        "log level" = "2";
       };
       scan = {
         path = "/space/documents/inbox";
